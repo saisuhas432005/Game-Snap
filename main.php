2dd71a0ed2a7afb47cf4fd384c5c8f5f8e88466e
kottanaindrakiran <kottanaindrakiran@gmail.com>
2025-02-07 18:43:07 +0530

    Add files via upload

---
+++ main.php
@@ -0,0 +1,89 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gaming Website Inspiration</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    
+    <header>
+        <nav class="navbar">
+        
+          <a href="#" class="logo">
+           
+            <img src="https://i.ibb.co/3mf10gJd/download-3-removebg-preview.png" alt="logo">
+            <h2>GAME SNAP</h2>
+          </a>
+          
+          <input type="text" id="searchBox" placeholder="Search games...">
+         <div class="searchButton"><button id="searchButton"  onclick="redirectToGame()">Search</button></div> 
+
+         
+       <a href="index.php"> <button class="login-btn" >LOG IN</button></a>   
+        </nav>
+      </header>
+    
+      
+    <section class="inspiration">
+       
+       
+        <div class="gallery">
+            <a href="actiongmaes.html">
+                <div class="card">
+                    
+                    <img src="https://i.ibb.co/4nhV22dG/movie-poster-movie-starring-actors-1158775-3480.jpg">
+                    <h3>ACTION GAMES</h3>
+                    <button>PLAY</button>
+                </div>
+            </a>
+            <a href="cargames.html">
+                <div class="card">
+                  
+                    <img src="https://i.ibb.co/5xG5Brt2/ios-racers-lead-asphalt-9.jpg">
+                    <h3>CAR GAMES</h3>
+                    <button>PLAY</button>
+                </div>
+            </a>
+            <!-- Add more cards as needed -->
+            <a href="bikegames.html">
+                <div class="card">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIjFdts3VdROprAP1lOG4hYjCz3OG7p1titg&s">
+                    <h3>BIKE GAMES</h3>
+                    <button>PLAY</button>
+                </div>
+            </a>
+            <a href="mindgames.html">
+                <div class="card">
+                    <img src="https://www.cubelelo.com/cdn/shop/articles/mind_games1.webp?v=1714472954">
+                    <h3>MIND GAMES</h3>
+                    <button>PLAY</button>
+                </div>
+            </a>
+            <a href="advaenturegames.html">
+                <div class="card">
+                    <img src="https://img.g2a.com/700x394/1x1x0/games-like-tomb-raider/6285445ff05e46d2bf4f977b">
+                    <h3>ADVAENTURE GAMES</h3>
+                    <button>PLAY</button>
+                </div>
+            </a>
+            <a href="couplegames.html">
+                <div class="card">
+                    <img src="https://img.freepik.com/premium-photo/young-couple-enjoying-video-game-night-together_1282444-95656.jpg">
+                    <h3>COUPLE GAMES</h3>
+                    <button>PLAY</button>
+                </div>
+            </a>
+           
+        </div>
+    </section>
+
+    <footer>
+        <p>&copy; 2025 KS. All rights reserved.</p>
+    </footer>
+
+    <script src="index.js"></script>
+</body>
+</html>
