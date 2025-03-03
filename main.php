bbc5545defc0d9dda52ea26e4d0514c01a72a142
kottanaindrakiran <kottanaindrakiran@gmail.com>
2025-03-03 21:29:39 +0530

    Update main.php

--- main.php
+++ main.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gaming Website Inspiration</title>
+    <title>Gaming Website </title>
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
